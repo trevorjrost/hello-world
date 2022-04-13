a42d6ed6945445500c0e6a7810c217d966d84a8c
trevorjrost <103599308+trevorjrost@users.noreply.github.com>
2022-04-13 18:46:49 +0000

    Add files via upload

---
+++ helloWorld.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello World!");
+   return 0;
+}
+
+//To run this command in terminal you will need to run the following command:
+//gcc hello-world.c -o hello-world-in-c
